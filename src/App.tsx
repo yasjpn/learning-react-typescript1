4dbe074afe8e531f8b82083c8af6097ccbd9385c
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-14 16:04:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+import { Todo } from "./Todo";
+
+import { TodoType } from "./types/todo";
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/Todo.tsx
@@ -0,0 +1,15 @@
+import { TodoType } from "./types/todo";
+
+export const Todo = (
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+  props: Omit<TodoType, "id">
+) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return (
+    <p>
+      {completeMark}
+      {title}(ユーザー: {userId})
+    </p>
+  );
+};
